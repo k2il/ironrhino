4e484f84cd09d95060f6af974dafc41db032ea1f
zhouyanming@gmail.com <zhouyanming@gmail.com@a2d4ebde-6050-0410-8aa7-e981bf2bc293>
2009-07-05 09:30:53 +0000

    split IndexAction

---
+++ ironrhino/src/org/ironrhino/online/action/RightAction.java
@@ -0,0 +1,58 @@
+package org.ironrhino.online.action;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Random;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.struts2.ServletActionContext;
+import org.ironrhino.common.util.RequestUtils;
+import org.ironrhino.core.annotation.AutoConfig;
+import org.ironrhino.core.ext.struts.BaseAction;
+import org.ironrhino.online.service.ProductFacade;
+import org.ironrhino.pms.model.Product;
+
+@AutoConfig(namespace = "/")
+public class RightAction extends BaseAction {
+
+	private ProductFacade productFacade;
+
+	private Product relatedProduct;
+
+	public Product getRelatedProduct() {
+		return relatedProduct;
+	}
+
+	public ProductFacade getProductFacade() {
+		return productFacade;
+	}
+
+	public void setProductFacade(ProductFacade productFacade) {
+		this.productFacade = productFacade;
+	}
+
+	public String execute() {
+		String history = RequestUtils.getCookieValue(ServletActionContext
+				.getRequest(), "HISTORY");
+		if (StringUtils.isNotBlank(history)) {
+			String[] array = history.split(",");
+			Random random = new Random();
+			int index = random.nextInt(array.length);
+			String code = array[index];
+			relatedProduct = productFacade.getProductByCode(code);
+			if (relatedProduct != null) {
+				Collection<Product> relatedProducts = relatedProduct
+						.getRelatedProducts();
+				if (relatedProducts.size() > 0) {
+					index = random.nextInt(relatedProducts.size());
+					Iterator<Product> it = relatedProducts.iterator();
+					for (int i = 0; i <= index; i++)
+						relatedProduct = it.next();
+				}
+			}
+		}
+		if (relatedProduct == null)
+			relatedProduct = productFacade.getRandomProduct();
+		return SUCCESS;
+	}
+}
