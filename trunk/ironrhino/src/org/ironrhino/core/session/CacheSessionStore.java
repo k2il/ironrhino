b3727bd8b833b714c36a74e57e7fc442fb199d3e
zhouyanming@gmail.com <zhouyanming@gmail.com@a2d4ebde-6050-0410-8aa7-e981bf2bc293>
2009-07-06 03:10:52 +0000

    refactor cache

--- trunk/ironrhino/src/org/ironrhino/core/session/CacheSessionStore.java
+++ trunk/ironrhino/src/org/ironrhino/core/session/CacheSessionStore.java
@@ -5,27 +5,27 @@ import java.util.Map;
 import net.sf.ehcache.Cache;
 import net.sf.ehcache.Element;
 
+import org.ironrhino.core.cache.CacheContext;
+
 public class CacheSessionStore implements SessionStore {
 
-	private Cache sessionCache;
-
-	public void setSessionCache(Cache sessionCache) {
-		this.sessionCache = sessionCache;
-	}
+	public static final String CACHE_NAME = "_session_";
 
 	public void initialize(Session session) {
+		Cache sessionCache = CacheContext.getCache(CACHE_NAME);
 		if (sessionCache.isKeyInCache(session.getId()))
 			session.setAttrMap((Map) sessionCache.get(session.getId())
 					.getValue());
 	}
 
 	public void save(Session session) {
-		sessionCache.put(new Element(session.getId(), session.getAttrMap(),
-				false, 15 * 60, -1));
+		CacheContext.getCache(CACHE_NAME).put(
+				new Element(session.getId(), session.getAttrMap(), false,
+						15 * 60, -1));
 	}
 
 	public void invalidate(Session session) {
-		sessionCache.remove(session.getId());
+		CacheContext.getCache(CACHE_NAME).remove(session.getId());
 	}
 
 }
