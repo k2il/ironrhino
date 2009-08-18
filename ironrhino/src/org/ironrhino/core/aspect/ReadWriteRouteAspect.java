9e389ff286cbd07bf34f4ba0b8b0bdd0ed54bdef
zhouyanming@gmail.com <zhouyanming@gmail.com@a2d4ebde-6050-0410-8aa7-e981bf2bc293>
2009-08-18 00:47:01 +0000

    refactor readonly again

--- ironrhino/src/org/ironrhino/core/aspect/ReadWriteRouteAspect.java
+++ ironrhino/src/org/ironrhino/core/aspect/ReadWriteRouteAspect.java
@@ -4,7 +4,6 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.ironrhino.core.ext.spring.ReadWriteRouteDataSource;
-import org.ironrhino.core.metadata.Readonly;
 import org.springframework.transaction.annotation.Transactional;
 
 /**
@@ -17,21 +16,11 @@ import org.springframework.transaction.annotation.Transactional;
 public class ReadWriteRouteAspect extends BaseAspect {
 
 	@Around("execution(public * *(..)) and @annotation(transactional)")
-	public Object determineReadonlyByTransactional(ProceedingJoinPoint jp,
+	public Object determineReadonly(ProceedingJoinPoint jp,
 			Transactional transactional) throws Throwable {
-		if (transactional != null && transactional.readOnly())
-			ReadWriteRouteDataSource.setReadonly(true);
-		try {
+		if(ReadWriteRouteDataSource.isReadonly())
 			return jp.proceed();
-		} finally {
-			ReadWriteRouteDataSource.setReadonly(false);
-		}
-	}
-
-	@Around("execution(public * *(..)) and @annotation(readonly)")
-	public Object determineReadonly(ProceedingJoinPoint jp, Readonly readonly)
-			throws Throwable {
-		if (readonly != null)
+		if (transactional != null && transactional.readOnly())
 			ReadWriteRouteDataSource.setReadonly(true);
 		try {
 			return jp.proceed();
