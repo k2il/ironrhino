20ffe2f8d28996115ca3c3169b803c7a926bde07
zhouyanming@gmail.com <zhouyanming@gmail.com@a2d4ebde-6050-0410-8aa7-e981bf2bc293>
2009-08-18 00:49:09 +0000

    refactor readonly again

--- ironrhino/src/org/ironrhino/core/aspect/ReadWriteRouteAspect.java
+++ ironrhino/src/org/ironrhino/core/aspect/ReadWriteRouteAspect.java
@@ -18,9 +18,9 @@ public class ReadWriteRouteAspect extends BaseAspect {
 	@Around("execution(public * *(..)) and @annotation(transactional)")
 	public Object determineReadonly(ProceedingJoinPoint jp,
 			Transactional transactional) throws Throwable {
-		if(ReadWriteRouteDataSource.isReadonly())
+		if (ReadWriteRouteDataSource.isReadonly())
 			return jp.proceed();
-		if (transactional != null && transactional.readOnly())
+		if (transactional.readOnly())
 			ReadWriteRouteDataSource.setReadonly(true);
 		try {
 			return jp.proceed();
