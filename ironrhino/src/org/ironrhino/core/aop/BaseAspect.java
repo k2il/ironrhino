316728a1ed0034e970ebf042a3c3e0ad7b5921fb
zhouyanming@gmail.com <zhouyanming@gmail.com@a2d4ebde-6050-0410-8aa7-e981bf2bc293>
2009-09-05 11:35:22 +0000

    refactor Aspect

--- ironrhino/src/org/ironrhino/core/aop/BaseAspect.java
+++ ironrhino/src/org/ironrhino/core/aop/BaseAspect.java
@@ -1,15 +1,34 @@
 package org.ironrhino.core.aop;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.aspectj.lang.JoinPoint;
 import org.mvel2.templates.TemplateRuntime;
 import org.springframework.core.Ordered;
 
 public class BaseAspect implements Ordered {
 
+	protected Log log = LogFactory.getLog(getClass());
+
 	private int order;
+
+	protected boolean isBypass() {
+		return AopContext.isBypass(this.getClass());
+	}
+
+	public int getOrder() {
+		return order;
+	}
+
+	public void setOrder(int order) {
+		this.order = order;
+	}
 
 	protected Object eval(String template, JoinPoint jp, Object retval) {
 		if (template == null)
@@ -24,20 +43,92 @@ public class BaseAspect implements Ordered {
 		if (retval != null)
 			context.put("retval", retval);
 		context.put("args", jp.getArgs());
-		Object value = TemplateRuntime.eval(template, context);
-		return value;
+		return TemplateRuntime.eval(template, context);
 	}
 
-	protected boolean isBypass() {
-		return AopContext.isBypass(this.getClass());
+	protected Object eval(String template, JoinPoint jp) {
+		return eval(template, jp, null);
 	}
 
-	public int getOrder() {
-		return order;
+	protected String evalString(String template, JoinPoint jp, Object retval) {
+		try {
+			Object obj = eval(template, jp, retval);
+			if (obj == null)
+				return null;
+			return obj.toString();
+		} catch (RuntimeException e) {
+			log.error(e.getMessage(), e);
+			return template;
+		}
 	}
 
-	public void setOrder(int order) {
-		this.order = order;
+	protected boolean evalBoolean(String template, JoinPoint jp, Object retval) {
+		try {
+			if (StringUtils.isBlank(template))
+				return true;
+			return Boolean.parseBoolean(evalString(template, jp, retval));
+		} catch (RuntimeException e) {
+			log.error(e.getMessage(), e);
+		}
+		return false;
 	}
 
+	protected int evalInt(String template, JoinPoint jp, Object retval) {
+		try {
+			Object obj = eval(template, jp, retval);
+			if (obj == null)
+				return 0;
+			if (obj instanceof Integer)
+				return (Integer) obj;
+			return Integer.parseInt(obj.toString());
+		} catch (RuntimeException e) {
+			log.error(e.getMessage(), e);
+			return 0;
+		}
+
+	}
+
+	protected long evalLong(String template, JoinPoint jp, Object retval) {
+		try {
+			Object obj = eval(template, jp, retval);
+			if (obj == null)
+				return 0;
+			if (obj instanceof Long)
+				return (Long) obj;
+			return Long.parseLong(obj.toString());
+		} catch (RuntimeException e) {
+			log.error(e.getMessage(), e);
+			return 0;
+		}
+
+	}
+
+	protected double evalDouble(String template, JoinPoint jp, Object retval) {
+		try {
+			Object obj = eval(template, jp, retval);
+			if (obj == null)
+				return 0;
+			if (obj instanceof Double)
+				return (Double) obj;
+			return Double.parseDouble(obj.toString());
+		} catch (RuntimeException e) {
+			log.error(e.getMessage(), e);
+			return 0;
+		}
+
+	}
+
+	protected List evalList(String template, JoinPoint jp, Object retval) {
+		try {
+			Object obj = eval(template, jp, retval);
+			if (obj == null)
+				return null;
+			if (obj instanceof List)
+				return (List) obj;
+			return Arrays.asList(obj.toString().split(","));
+		} catch (RuntimeException e) {
+			log.error(e.getMessage(), e);
+			return null;
+		}
+	}
 }
