ccdf49ecf1add5b77bac1ba40802384fe80137e1
zhouyanming@gmail.com <zhouyanming@gmail.com@a2d4ebde-6050-0410-8aa7-e981bf2bc293>
2009-09-11 03:11:44 +0000

    remove dwr

---
+++ trunk/ironrhino/src/org/ironrhino/common/action/ConsoleAction.java
@@ -0,0 +1,48 @@
+package org.ironrhino.common.action;
+
+import org.ironrhino.core.ext.spring.ApplicationContextConsole;
+import org.ironrhino.core.ext.struts.BaseAction;
+import org.ironrhino.core.metadata.AutoConfig;
+
+import com.opensymphony.xwork2.interceptor.annotations.InputConfig;
+import com.opensymphony.xwork2.validator.annotations.RequiredStringValidator;
+import com.opensymphony.xwork2.validator.annotations.Validations;
+import com.opensymphony.xwork2.validator.annotations.ValidatorType;
+
+@AutoConfig
+public class ConsoleAction extends BaseAction {
+
+	private static final long serialVersionUID = 8180265410790553918L;
+
+	private String cmd;
+
+	private transient ApplicationContextConsole applicationContextConsole;
+
+	public String getCmd() {
+		return cmd;
+	}
+
+	public void setCmd(String cmd) {
+		this.cmd = cmd;
+	}
+
+	public void setApplicationContextConsole(
+			ApplicationContextConsole applicationContextConsole) {
+		this.applicationContextConsole = applicationContextConsole;
+	}
+
+	@Override
+	@InputConfig(resultName = "success")
+	@Validations(requiredStrings = { @RequiredStringValidator(type = ValidatorType.FIELD, fieldName = "cmd", trim = true, key = "validation.required") })
+	public String execute() {
+		try {
+			Object o = applicationContextConsole.execute(cmd);
+			addActionMessage(getText("operate.success") + ":"
+					+ String.valueOf(o));
+		} catch (Exception e) {
+			addActionError(getText("error") + ":" + e.getMessage());
+		}
+		return SUCCESS;
+	}
+
+}
