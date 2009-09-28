fb46b824d93a38e7e91e6ec0fbd1365c0198ab05
zhouyanming@gmail.com <zhouyanming@gmail.com@a2d4ebde-6050-0410-8aa7-e981bf2bc293>
2009-09-28 02:19:23 +0000

    move httpWrappedSession.save() back to HttpSessionFilter

--- trunk/ironrhino/src/org/ironrhino/core/session/HttpSessionFilter.java
+++ trunk/ironrhino/src/org/ironrhino/core/session/HttpSessionFilter.java
@@ -7,24 +7,20 @@ import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import org.ironrhino.core.performance.BufferableResponseWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
-/**
- * notice HttpWrappedSession.save() called before sitemesh apply decorator
- * 
- * @see org.ironrhino.core.struts.HookedFreeMarkerPageFilter
- * @author minggao
- * 
- */
 @Component("httpSessionFilter")
 public class HttpSessionFilter implements Filter {
 
@@ -45,7 +41,8 @@ public class HttpSessionFilter implements Filter {
 				(HttpServletResponse) response, servletContext);
 		HttpWrappedRequest httpRequest = new HttpWrappedRequest(req,
 				httpContext, httpSessionManager);
-
+		BufferableResponseWrapper res = new BufferableResponseWrapper(
+				(HttpServletResponse) response);
 		// copy from
 		// org.springframework.web.context.request.ServletRequestListener
 		ServletRequestAttributes attributes = new ServletRequestAttributes(
@@ -53,7 +50,7 @@ public class HttpSessionFilter implements Filter {
 		LocaleContextHolder.setLocale(request.getLocale());
 		RequestContextHolder.setRequestAttributes(attributes);
 
-		chain.doFilter(httpRequest, response);
+		chain.doFilter(httpRequest, res);
 
 		// copy from
 		// org.springframework.web.context.request.ServletRequestListener
@@ -69,6 +66,23 @@ public class HttpSessionFilter implements Filter {
 		}
 		if (attributes != null) {
 			attributes.requestCompleted();
+		}
+		try {
+			byte[] bytes = res.getContents();
+			if (bytes == null)
+				bytes = new byte[0];
+			HttpSession session = httpRequest.getSession();
+			if (session instanceof HttpWrappedSession) {
+				((HttpWrappedSession) session).save();
+			}
+			response.setContentLength(bytes.length);
+			ServletOutputStream sos = response.getOutputStream();
+			if (bytes.length > 0)
+				sos.write(bytes);
+			sos.flush();
+			sos.close();
+		} catch (IllegalStateException e) {
+			e.printStackTrace();
 		}
 	}
 
--- trunk/ironrhino/src/org/ironrhino/core/struts/HookedFreeMarkerPageFilter.java
+++ trunk/ironrhino/src/org/ironrhino/core/struts/HookedFreeMarkerPageFilter.java
@@ -1,24 +1,3 @@
-/*
- * $Id: FreeMarkerPageFilter.java 651946 2008-04-27 13:41:38Z apetrelli $
- *
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
 package org.ironrhino.core.struts;
 
 import java.io.IOException;
@@ -27,11 +6,9 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.struts2.sitemesh.FreeMarkerPageFilter;
-import org.ironrhino.core.session.HttpWrappedSession;
 import org.ironrhino.core.util.HtmlUtils;
 
 import com.opensymphony.module.sitemesh.Decorator;
@@ -44,7 +21,6 @@ public class HookedFreeMarkerPageFilter extends FreeMarkerPageFilter {
 			HttpServletRequest req, HttpServletResponse res,
 			ServletContext servletContext, ActionContext ctx)
 			throws ServletException, IOException {
-		saveSession(page, decorator, req, res, servletContext, ctx);
 		String replacement = req.getParameter("_replacement_");
 		if (StringUtils.isNotBlank(replacement)) {
 			compressBody(page, decorator, req, res, servletContext, ctx);
@@ -53,15 +29,6 @@ public class HookedFreeMarkerPageFilter extends FreeMarkerPageFilter {
 					.applyDecorator(page, decorator, req, res, servletContext,
 							ctx);
 		}
-	}
-
-	protected void saveSession(Page page, Decorator decorator,
-			HttpServletRequest req, HttpServletResponse res,
-			ServletContext servletContext, ActionContext ctx) {
-		HttpSession session = req.getSession();
-		if (session instanceof HttpWrappedSession)
-			((HttpWrappedSession) session).save();
-
 	}
 
 	protected void compressBody(Page page, Decorator decorator,
