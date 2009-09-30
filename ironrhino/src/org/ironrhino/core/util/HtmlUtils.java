75e95e2dd44342a6a4cf9c8da79a87cbb2e5224f
zhouyanming@gmail.com <zhouyanming@gmail.com@a2d4ebde-6050-0410-8aa7-e981bf2bc293>
2009-09-30 06:05:57 +0000

    remove unused jars

--- ironrhino/src/org/ironrhino/core/util/HtmlUtils.java
+++ ironrhino/src/org/ironrhino/core/util/HtmlUtils.java
@@ -6,16 +6,8 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Vector;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import org.apache.commons.lang.StringEscapeUtils;
-import org.apache.oro.text.regex.PatternCompiler;
-import org.apache.oro.text.regex.PatternMatcher;
-import org.apache.oro.text.regex.Perl5Compiler;
-import org.apache.oro.text.regex.Perl5Matcher;
-import org.apache.oro.text.regex.Perl5Substitution;
-import org.apache.oro.text.regex.Util;
 import org.htmlparser.Attribute;
 import org.htmlparser.Node;
 import org.htmlparser.NodeFilter;
@@ -27,11 +19,6 @@ import org.htmlparser.util.NodeIterator;
 import org.ironrhino.core.model.BaseTreeableEntity;
 
 public class HtmlUtils {
-	static final Pattern SCRIPT_TAG_PATTERN = Pattern.compile(
-			"<script[^>]*>.*</script[^>]*>", Pattern.CASE_INSENSITIVE);
-
-	static final PatternCompiler pc = new Perl5Compiler();
-	static final PatternMatcher matcher = new Perl5Matcher();
 
 	public static String getTreeViewHtml(Collection children, boolean async) {
 		if (async || children == null || children.size() == 0)
@@ -132,200 +119,6 @@ public class HtmlUtils {
 			for (int i = 0; i < node.getChildren().size(); i++)
 				escapeHtml(node.getChildren().elementAt(i));
 
-	}
-
-	// http://www.bitscn.com/hack/young/200708/108278.html
-	public static String antiXss(String content) {
-		String old = content;
-		String ret = _antiXss(content);
-		while (!ret.equals(old)) {
-			old = ret;
-			ret = _antiXss(ret);
-		}
-		return tidy(ret);
-	}
-
-	/**
-	 * �Ƿ�xss����
-	 */
-	public static boolean hasXss(String content) {
-		try {
-			return hasScriptTag(content) || hasEvent(content)
-					|| hasAsciiAndHex(content) || hasCssExpression(content)
-					|| hasProtocol(content) || hasAllowScriptAccess(content);
-		} catch (Exception e) {
-			e.printStackTrace();
-			return true;
-		}
-	}
-
-	private static String _antiXss(String content) {
-		try {
-			return stripAllowScriptAccess(stripProtocol(stripCssExpression(stripAsciiAndHex(stripEvent(stripScriptTag(content))))));
-		} catch (Exception e) {
-			e.printStackTrace();
-			return null;
-		}
-	}
-
-	private static String stripScriptTag(String content) {
-		Matcher m = SCRIPT_TAG_PATTERN.matcher(content);
-		content = m.replaceAll("");
-		return content;
-	}
-
-	private static boolean hasScriptTag(String content) {
-		Matcher m = SCRIPT_TAG_PATTERN.matcher(content);
-		return m.find();
-	}
-
-	private static String stripEvent(String content) throws Exception {
-		String[] events = { "onmouseover", "onmouseout", "onmousedown",
-				"onmouseup", "onmousemove", "onclick", "ondblclick",
-				"onkeypress", "onkeydown", "onkeyup", "ondragstart",
-				"onerrorupdate", "onhelp", "onreadystatechange", "onrowenter",
-				"onrowexit", "onselectstart", "onload", "onunload",
-				"onbeforeunload", "onblur", "onerror", "onfocus", "onresize",
-				"onscroll", "oncontextmenu" };
-		for (String event : events) {
-			org.apache.oro.text.regex.Pattern p = pc.compile("(<[^>]*)("
-					+ event + ")([^>]*>)", Perl5Compiler.CASE_INSENSITIVE_MASK);
-			content = Util.substitute(matcher, p, new Perl5Substitution("$1"
-					+ event.substring(2) + "$3"), content, Util.SUBSTITUTE_ALL);
-
-		}
-		return content;
-	}
-
-	private static boolean hasEvent(String content) throws Exception {
-		String[] events = { "onmouseover", "onmouseout", "onmousedown",
-				"onmouseup", "onmousemove", "onclick", "ondblclick",
-				"onkeypress", "onkeydown", "onkeyup", "ondragstart",
-				"onerrorupdate", "onhelp", "onreadystatechange", "onrowenter",
-				"onrowexit", "onselectstart", "onload", "onunload",
-				"onbeforeunload", "onblur", "onerror", "onfocus", "onresize",
-				"onscroll", "oncontextmenu" };
-		for (String event : events) {
-			org.apache.oro.text.regex.Pattern p = pc.compile("(<[^>]*)("
-					+ event + ")([^>]*>)", Perl5Compiler.CASE_INSENSITIVE_MASK);
-			if (matcher.contains(content, p))
-				return true;
-		}
-		return false;
-	}
-
-	private static String stripAsciiAndHex(String content) throws Exception {
-		org.apache.oro.text.regex.Pattern p = pc.compile(
-				"(<[^>]*)(&#|\\\\00)([^>]*>)",
-				Perl5Compiler.CASE_INSENSITIVE_MASK);
-		content = Util.substitute(matcher, p, new Perl5Substitution("$1$3"),
-				content, Util.SUBSTITUTE_ALL);
-		return content;
-	}
-
-	private static boolean hasAsciiAndHex(String content) throws Exception {
-		org.apache.oro.text.regex.Pattern p = pc.compile(
-				"(<[^>]*)(&#|\\\\00)([^>]*>)",
-				Perl5Compiler.CASE_INSENSITIVE_MASK);
-		return matcher.contains(content, p);
-	}
-
-	private static String stripCssExpression(String content) throws Exception {
-		org.apache.oro.text.regex.Pattern p = pc.compile(
-				"(<[^>]*style=.*)/\\*.*\\*/([^>]*>)",
-				Perl5Compiler.CASE_INSENSITIVE_MASK);
-		content = Util.substitute(matcher, p, new Perl5Substitution("$1$2"),
-				content, Util.SUBSTITUTE_ALL);
-
-		p = pc
-				.compile(
-						"(<[^>]*style=[^>]+)(expression|javascript|vbscript|-moz-binding)([^>]*>)",
-						Perl5Compiler.CASE_INSENSITIVE_MASK);
-		content = Util.substitute(matcher, p, new Perl5Substitution("$1$3"),
-				content, Util.SUBSTITUTE_ALL);
-
-		p = pc.compile("(<style[^>]*>.*)/\\*.*\\*/(.*</style[^>]*>)",
-				Perl5Compiler.CASE_INSENSITIVE_MASK);
-		content = Util.substitute(matcher, p, new Perl5Substitution("$1$2"),
-				content, Util.SUBSTITUTE_ALL);
-
-		p = pc
-				.compile(
-						"(<style[^>]*>[^>]+)(expression|javascript|vbscript|-moz-binding)(.*</style[^>]*>)",
-						Perl5Compiler.CASE_INSENSITIVE_MASK);
-		content = Util.substitute(matcher, p, new Perl5Substitution("$1$3"),
-				content, Util.SUBSTITUTE_ALL);
-		return content;
-	}
-
-	private static boolean hasCssExpression(String content) throws Exception {
-		org.apache.oro.text.regex.Pattern p = pc.compile(
-				"(<[^>]*style=.*)/\\*.*\\*/([^>]*>)",
-				Perl5Compiler.CASE_INSENSITIVE_MASK);
-		if (matcher.contains(content, p))
-			return true;
-		p = pc
-				.compile(
-						"(<[^>]*style=[^>]+)(expression|javascript|vbscript|-moz-binding)([^>]*>)",
-						Perl5Compiler.CASE_INSENSITIVE_MASK);
-		if (matcher.contains(content, p))
-			return true;
-		p = pc.compile("(<style[^>]*>.*)/\\*.*\\*/(.*</style[^>]*>)",
-				Perl5Compiler.CASE_INSENSITIVE_MASK);
-		if (matcher.contains(content, p))
-			return true;
-		p = pc
-				.compile(
-						"(<style[^>]*>[^>]+)(expression|javascript|vbscript|-moz-binding)(.*</style[^>]*>)",
-						Perl5Compiler.CASE_INSENSITIVE_MASK);
-		if (matcher.contains(content, p))
-			return true;
-		return false;
-	}
-
-	private static String stripProtocol(String content) throws Exception {
-		String[] protocols = { "javascript", "vbscript", "livescript",
-				"ms-its", "mhtml", "data", "firefoxurl", "mocha" };
-		for (String protocol : protocols) {
-			org.apache.oro.text.regex.Pattern p = pc.compile("(<[^>]*)"
-					+ protocol + ":([^>]*>)",
-					Perl5Compiler.CASE_INSENSITIVE_MASK);
-			content = Util.substitute(matcher, p,
-					new Perl5Substitution("$1/$2"), content,
-					Util.SUBSTITUTE_ALL);
-		}
-		return content;
-	}
-
-	private static boolean hasProtocol(String content) throws Exception {
-		String[] protocols = { "javascript", "vbscript", "livescript",
-				"ms-its", "mhtml", "data", "firefoxurl", "mocha" };
-		for (String protocol : protocols) {
-			org.apache.oro.text.regex.Pattern p = pc.compile("(<[^>]*)"
-					+ protocol + ":([^>]*>)",
-					Perl5Compiler.CASE_INSENSITIVE_MASK);
-			if (matcher.contains(content, p))
-				return true;
-		}
-		return false;
-	}
-
-	private static String stripAllowScriptAccess(String content)
-			throws Exception {
-		org.apache.oro.text.regex.Pattern p = pc.compile(
-				"(<[^>]*)AllowScriptAccess([^>]*>)",
-				Perl5Compiler.CASE_INSENSITIVE_MASK);
-		content = Util.substitute(matcher, p, new Perl5Substitution(
-				"$1Allow_Script_Access$2"), content, Util.SUBSTITUTE_ALL);
-		return content;
-	}
-
-	private static boolean hasAllowScriptAccess(String content)
-			throws Exception {
-		org.apache.oro.text.regex.Pattern p = pc.compile(
-				"(<[^>]*)AllowScriptAccess([^>]*>)",
-				Perl5Compiler.CASE_INSENSITIVE_MASK);
-		return matcher.contains(content, p);
 	}
 
 }
