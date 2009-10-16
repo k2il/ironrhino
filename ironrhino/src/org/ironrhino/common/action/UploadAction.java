fb5f6d27ebe1cc5fd1d9ff403c643cc30b76e4ad
zhouyanming@gmail.com <zhouyanming@gmail.com@a2d4ebde-6050-0410-8aa7-e981bf2bc293>
2009-10-16 07:29:13 +0000

    use hdfs as default fs

--- ironrhino/src/org/ironrhino/common/action/UploadAction.java
+++ ironrhino/src/org/ironrhino/common/action/UploadAction.java
@@ -1,6 +1,7 @@
 package org.ironrhino.common.action;
 
 import java.io.File;
+import java.util.UUID;
 
 import org.ironrhino.core.fs.FileStorage;
 import org.ironrhino.core.metadata.AutoConfig;
@@ -14,26 +15,32 @@ public class UploadAction extends BaseAction {
 
 	private File[] file;
 
-	private String[] filenames;
+	private String[] fileFileName;
 
 	@Autowired
 	private transient FileStorage fileStorage;
-
-	public String[] getFilenames() {
-		return filenames;
-	}
 
 	public void setFile(File[] file) {
 		this.file = file;
 	}
 
+	public void setFileFileName(String[] fileFileName) {
+		this.fileFileName = fileFileName;
+	}
+
 	public String execute() {
 		if (file != null) {
-			filenames = new String[file.length];
 			int i = 0;
-			for (File f : file)
-				filenames[i++] = fileStorage.save(f);
+			for (File f : file) {
+				fileStorage.save(f, createPath(fileFileName[i]));
+				i++;
+			}
 		}
 		return NONE;
+	}
+
+	private String createPath(String filename) {
+		return "/upload/" + UUID.randomUUID().toString().replaceAll("-", "")
+				+ "-" + filename;
 	}
 }
