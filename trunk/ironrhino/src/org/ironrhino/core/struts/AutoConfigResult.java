475e4360c0b58484b13e8d1ab78ce6df89ec85a8
zhouyanming <zhouyanming@a2d4ebde-6050-0410-8aa7-e981bf2bc293>
2011-03-30 08:28:43 +0000

    remove jsp support

--- trunk/ironrhino/src/org/ironrhino/core/struts/AutoConfigResult.java
+++ trunk/ironrhino/src/org/ironrhino/core/struts/AutoConfigResult.java
@@ -12,7 +12,6 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.lang.StringUtils;
 import org.apache.struts2.ServletActionContext;
 import org.apache.struts2.StrutsStatics;
-import org.apache.struts2.dispatcher.ServletDispatcherResult;
 import org.apache.struts2.views.freemarker.FreemarkerResult;
 import org.ironrhino.core.util.AppInfo;
 import org.ironrhino.core.util.AppInfo.Stage;
@@ -27,16 +26,9 @@ public class AutoConfigResult extends FreemarkerResult {
 
 	private static final long serialVersionUID = -2277156996891287055L;
 
-	public static final String DEFAULT_JSP_LOCATION = "/WEB-INF/view/jsp";
-
 	public static final String DEFAULT_FTL_LOCATION = "/WEB-INF/view/ftl";
 
 	public static final String DEFAULT_FTL_CLASSPATH = "/resources/view";
-
-	private static ServletDispatcherResult servletDispatcherResult = new ServletDispatcherResult();
-
-	@Inject(value = "ironrhino.view.jsp.location", required = false)
-	private static String jspLocation = DEFAULT_JSP_LOCATION;
 
 	@Inject(value = "ironrhino.view.ftl.location", required = false)
 	private static String ftlLocation = DEFAULT_FTL_LOCATION;
@@ -63,10 +55,7 @@ public class AutoConfigResult extends FreemarkerResult {
 			return;
 		}
 		String finalLocation = conditionalParse(location, invocation);
-		if (finalLocation.endsWith(".jsp"))
-			servletDispatcherResult.doExecute(finalLocation, invocation);
-		else
-			doExecute(finalLocation, invocation);
+		doExecute(finalLocation, invocation);
 	}
 
 	private static Map<String, String> cache = new ConcurrentHashMap<String, String>(
@@ -86,29 +75,18 @@ public class AutoConfigResult extends FreemarkerResult {
 			URL url = null;
 			location = getTemplateLocation(templateName);
 			if (location == null) {
-				location = new StringBuilder().append(jspLocation).append(
-						"/meta/result/").append(result).append(".jsp")
+				location = new StringBuilder().append(ftlLocation)
+						.append("/meta/result/").append(result).append(".ftl")
 						.toString();
 				try {
 					url = context.getResource(location);
 				} catch (MalformedURLException e) {
 					e.printStackTrace();
 				}
-
-				if (url == null) {
-					location = new StringBuilder().append(ftlLocation).append(
-							"/meta/result/").append(result).append(".ftl")
-							.toString();
-					try {
-						url = context.getResource(location);
-					} catch (MalformedURLException e) {
-						e.printStackTrace();
-					}
-				}
 				if (url == null)
-					location = new StringBuilder().append(ftlClasspath).append(
-							"/meta/result/").append(result).append(".ftl")
-							.toString();
+					location = new StringBuilder().append(ftlClasspath)
+							.append("/meta/result/").append(result)
+							.append(".ftl").toString();
 			}
 			if (AppInfo.getStage() == Stage.PRODUCTION)
 				cache.put(templateName, location);
@@ -121,25 +99,16 @@ public class AutoConfigResult extends FreemarkerResult {
 		if (location == null || AppInfo.getStage() == AppInfo.Stage.DEVELOPMENT) {
 			ServletContext context = ServletActionContext.getServletContext();
 			URL url = null;
-			location = new StringBuilder().append(jspLocation).append(
-					templateName).append(".jsp").toString();
+			location = new StringBuilder().append(ftlLocation)
+					.append(templateName).append(".ftl").toString();
 			try {
 				url = context.getResource(location);
 			} catch (MalformedURLException e) {
 				e.printStackTrace();
 			}
 			if (url == null) {
-				location = new StringBuilder().append(ftlLocation).append(
-						templateName).append(".ftl").toString();
-				try {
-					url = context.getResource(location);
-				} catch (MalformedURLException e) {
-					e.printStackTrace();
-				}
-			}
-			if (url == null) {
-				location = new StringBuilder().append(ftlClasspath).append(
-						templateName).append(".ftl").toString();
+				location = new StringBuilder().append(ftlClasspath)
+						.append(templateName).append(".ftl").toString();
 				url = ClassLoaderUtil.getResource(location.substring(1),
 						AutoConfigResult.class);
 			}
